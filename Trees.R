757d11098179e27e35f71e33555ca8199c4e71f1
S-Myles <shannon.myles@dal.ca>
2024-12-17 17:52:34 -0500

    Trees and taxonomies adapted for comparisons between blastn and qiime tax

--- Trees.R
+++ Trees.R
@@ -1,3 +1,4 @@
+library(ape)
 ########
 # 12S
 ########
@@ -7,12 +8,12 @@
 physeq <- merge_phyloseq(S12_physeq_data, 
                          phy_tree(read.tree("data/12S_aligned_masked_tree_rooted.nwk")))
 
-# plot tree
+# plot tree with qiime classified data
 (p <- plot_tree(physeq, 
                 color = "Order", 
                 label.tips = "Species", 
                 size = "abundance", 
-                shape = "Season",
+                shape = "Size.Fraction",
                 justify = "left") +
     theme(legend.position = "right"))
 
@@ -27,12 +28,54 @@ physeq <- tax_glom(physeq, taxrank = "Species")
                 color = "Order", 
                 label.tips = "Species", 
                 size = "abundance", 
-                shape = "Season") +
+                shape = "Size.Fraction") +
                 #justify = "left") +
     theme(legend.position = "right"))
 
 ggsave("outputs/Trees/12S-Species_tree.png", 
-       plot = p, width = 6, height = 12)
+       plot = p, width = 10, height = 14)
+
+#########
+# Now plot same tree with Blastn results
+# Blast tree needs new taxonomy
+# Taxonomy reference table
+S12_taxonomy <- read_tsv("./data/12S_blastn_taxized.txt") %>%
+  column_to_rownames('ASV_ID') %>%
+  as.matrix()
+
+
+S12_blast_physeq <- merge_phyloseq(otu_table(S12_physeq_data), 
+                                   sample_data(S12_physeq_data), 
+                                   tax_table(S12_taxonomy),
+                                   phy_tree(read.tree("data/12S_aligned_masked_tree_rooted.nwk")))
+
+# plot tree with qiime classified data
+(p <- plot_tree(S12_blast_physeq, 
+                color = "Order", 
+                label.tips = "Species", 
+                size = "abundance", 
+                shape = "Size.Fraction",
+                justify = "left") +
+    theme(legend.position = "right"))
+
+ggsave("outputs/Trees/12S-ASV_BLAST_tree.pdf", 
+       plot = p, width = 22, height = 49)
+
+# Optionally aggregate dataset
+physeq <- tax_glom(S12_blast_physeq, taxrank = "Species")
+
+# plot tree
+(p <- plot_tree(S12_blast_physeq, 
+                color = "Order", 
+                label.tips = "Species", 
+                size = "abundance", 
+                shape = "Size.Fraction") +
+    #justify = "left") +
+    theme(legend.position = "right"))
+
+ggsave("outputs/Trees/12S-Species_BLAST_tree.png", 
+       plot = p, width = 10, height = 14)
+
 
 
 
@@ -46,14 +89,14 @@ ggsave("outputs/Trees/12S-Species_tree.png",
 ########
 # Add tree to dataset
 physeq <- merge_phyloseq(S16_physeq_data, 
-                         phy_tree(read.tree("data/16S_aligned_masked_tree_rooted.nwk")))
+                         phy_tree(read.tree("data/16S_filt_aligned_masked_tree_rooted.nwk")))
 
 # plot tree
 (p <- plot_tree(physeq, 
                 color = "Phylum", 
                 #label.tips = "Species", 
                 size = "abundance", 
-                shape = "Season",
+                shape = "Size.Fraction",
                 justify = "left") +
     theme(legend.position = "right"))
 
@@ -68,12 +111,12 @@ physeq <- tax_glom(physeq, taxrank = "Phylum")
                 color = "Phylum", 
                 label.tips = "Phylum", 
                 size = "abundance", 
-                shape = "Season")+
+                shape = "Size.Fraction")+
                 #justify = "left") +
     theme(legend.position = "right"))
 
 ggsave("outputs/Trees/16S-Phylum_tree.png", 
-       plot = p, width = 16, height = 22)
+       plot = p, width = 10, height = 14)
 
 
 
@@ -86,13 +129,13 @@ ggsave("outputs/Trees/16S-Phylum_tree.png",
 ########
 # Add tree to dataset
 physeq <- merge_phyloseq(S18_physeq_data, 
-                         phy_tree(read.tree("data/18S_aligned_masked_tree_rooted.nwk")))
+                         phy_tree(read.tree("data/18S_filt_aligned_masked_tree_rooted.nwk")))
 # plot tree
 (p <- plot_tree(physeq, 
                 color = "Phylum", 
                 label.tips = "Species", 
                 size = "abundance", 
-                shape = "Season",
+                shape = "Size.Fraction",
                 justify = "left") +
     theme(legend.position = "right"))
 
@@ -107,12 +150,12 @@ physeq <- tax_glom(physeq, taxrank = "Phylum")
                 color = "Phylum", 
                 label.tips = "Phylum", 
                 size = "abundance", 
-                shape = "Season") +
+                shape = "Size.Fraction") +
                 #justify = "left") +
     theme(legend.position = "right"))
 
 ggsave("outputs/Trees/18S-Phylum_tree.png", 
-       plot = p, width = 10, height = 20)
+       plot = p, width = 10, height = 14)
 
 
 
@@ -124,14 +167,14 @@ ggsave("outputs/Trees/18S-Phylum_tree.png",
 ########
 # Add tree to dataset
 physeq <- merge_phyloseq(COI_physeq_data, 
-                         phy_tree(read.tree("data/COI_aligned_masked_tree_rooted.nwk")))
+                         phy_tree(read.tree("data/COI_filt_aligned_masked_tree_rooted.nwk")))
 
 # plot tree
 (p <- plot_tree(physeq, 
                 color = "Phylum", 
                 label.tips = "Species", 
                 size = "abundance", 
-                shape = "Season",
+                shape = "Size.Fraction",
                 justify = "left") +
     theme(legend.position = "right"))
 
@@ -146,9 +189,52 @@ physeq <- tax_glom(physeq, taxrank = "Phylum")
                 color = "Phylum", 
                 label.tips = "Phylum", 
                 size = "abundance", 
-                shape = "Season") +
+                shape = "Size.Fraction") +
                 #justify = "left") +
     theme(legend.position = "right"))
 
 ggsave("outputs/Trees/COI-Phylum_tree.png", 
-       plot = p, width = 6, height = 12)
+       plot = p, width = 10, height = 14)
+
+
+#########
+# Now plot same tree with Blastn results
+# Blast tree needs new taxonomy
+# Taxonomy reference table
+COI_taxonomy <- read_tsv("./data/COI_blastn_taxized.txt") %>%
+  column_to_rownames('Feature ID') %>%
+  as.matrix()
+
+
+COI_blast_physeq <- merge_phyloseq(otu_table(COI_physeq_data), 
+                                   sample_data(COI_physeq_data), 
+                                   tax_table(COI_taxonomy),
+                                   phy_tree(read.tree("data/COI_filt_aligned_masked_tree_rooted.nwk")))
+
+# plot tree with qiime classified data
+(p <- plot_tree(COI_blast_physeq, 
+                color = "Phylum", 
+                label.tips = "Species", 
+                size = "abundance", 
+                shape = "Size.Fraction",
+                justify = "left") +
+    theme(legend.position = "right"))
+
+ggsave("outputs/Trees/COI-ASV_BLAST_tree.pdf", 
+       plot = p, width = 22, height = 49)
+
+# Optionally aggregate dataset
+physeq <- tax_glom(COI_blast_physeq, taxrank = "Phylum")
+
+
+# plot tree
+(p <- plot_tree(physeq, 
+                color = "Phylum", 
+                label.tips = "Phylum", 
+                size = "abundance", 
+                shape = "Size.Fraction") +
+    #justify = "left") +
+    theme(legend.position = "right"))
+
+ggsave("outputs/Trees/COI-Phylum_BLAST_tree.png", 
+       plot = p, width = 10, height = 14)
