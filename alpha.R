0b21849520d30d47e50a5b9100ccedc020aa3f82
S-Myles <shannon.myles@dal.ca>
2025-01-06 17:01:17 -0500

    Produced alphas

--- alpha.R
+++ alpha.R
@@ -7,7 +7,7 @@ library(microbiome)   # Contains alpha diversity metrics calculation function
 (sample_sum_df <- data.frame(sum = sample_sums(S12_physeq_data)) %>% 
    arrange(desc(sum)))
 
-(S12_rarefied_data <- rarefy_even_depth(S12_physeq_data, sample.size = min(sample_sums(S12_physeq_data)),
+(S12_rarefied_data <- rarefy_even_depth(S12_physeq_data, sample.size = 5000,
                   rngseed = 199, replace = FALSE, trimOTUs = TRUE, verbose = TRUE))
 
 
@@ -21,11 +21,11 @@ mean(S12_alpha$Chao1)
 mean(S12_alpha$Shannon)
 mean(S12_alpha$Pielou)
 
-(plot <- ggplot(S12_alpha, aes(x = Season, y = Chao1, color = Station)) +
+(plot <- ggplot(S12_alpha, aes(x = Season, y = Chao1, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Chao1), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S12_alpha$Chao1), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(S12_alpha$Chao1) - 12, mean(S12_alpha$Chao1) + 12) + # Set y-axis limits
+    ylim(mean(S12_alpha$Chao1) - 22, mean(S12_alpha$Chao1) + 22) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -42,7 +42,7 @@ ggsave(
   plot = plot, width = 6, height = 6, dpi = 300
 )
 
-(plot <- ggplot(S12_alpha, aes(x = Season, y = Shannon, color = Station)) +
+(plot <- ggplot(S12_alpha, aes(x = Season, y = Shannon, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Shannon), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S12_alpha$Shannon), linetype = "dashed", color = "red", size = 1) + # Add mean line
@@ -63,11 +63,11 @@ ggsave(
   plot = plot, width = 6, height = 6, dpi = 300
 )
 
-(plot <- ggplot(S12_alpha, aes(x = Season, y = Pielou, color = Station)) +
+(plot <- ggplot(S12_alpha, aes(x = Season, y = Pielou, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Pielou), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S12_alpha$Pielou), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(S12_alpha$Pielou) - 0.25, mean(S12_alpha$Pielou) + 0.25) + # Set y-axis limits
+    ylim(mean(S12_alpha$Pielou) - 0.3, mean(S12_alpha$Pielou) + 0.3) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -109,11 +109,11 @@ mean(S16_alpha$Shannon)
 mean(S16_alpha$Pielou)
 
 
-(plot <- ggplot(S16_alpha, aes(x = Season, y = Chao1, color = Station)) +
+(plot <- ggplot(S16_alpha, aes(x = Season, y = Chao1, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Chao1), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S16_alpha$Chao1), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(S16_alpha$Chao1) - 550, mean(S16_alpha$Chao1) + 550) + # Set y-axis limits
+    ylim(mean(S16_alpha$Chao1) - 590, mean(S16_alpha$Chao1) + 590) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -130,11 +130,11 @@ ggsave(
   plot = plot, width = 6, height = 6, dpi = 300
 )
 
-(plot <- ggplot(S16_alpha, aes(x = Season, y = Shannon, color = Station)) +
+(plot <- ggplot(S16_alpha, aes(x = Season, y = Shannon, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Shannon), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S16_alpha$Shannon), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(S16_alpha$Shannon) - 0.8, mean(S16_alpha$Shannon) + 0.8) + # Set y-axis limits
+    ylim(mean(S16_alpha$Shannon) - 1.1, mean(S16_alpha$Shannon) + 1.1) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -151,11 +151,11 @@ ggsave(
   plot = plot, width = 6, height = 6, dpi = 300
 )
 
-(plot <- ggplot(S16_alpha, aes(x = Season, y = Pielou, color = Station)) +
+(plot <- ggplot(S16_alpha, aes(x = Season, y = Pielou, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Pielou), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S16_alpha$Pielou), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(S16_alpha$Pielou) - 0.1, mean(S16_alpha$Pielou) + 0.1) + # Set y-axis limits
+    ylim(mean(S16_alpha$Pielou) - 0.18, mean(S16_alpha$Pielou) + 0.18) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -184,7 +184,7 @@ ggsave(
 (sample_sum_df <- data.frame(sum = sample_sums(S18_physeq_data)) %>% 
    arrange(desc(sum)))
 
-(S18_rarefied_data <- rarefy_even_depth(S18_physeq_data, sample.size = 4000,
+(S18_rarefied_data <- rarefy_even_depth(S18_physeq_data, sample.size = 5000,
                                         rngseed = 199, replace = FALSE, trimOTUs = TRUE, verbose = TRUE))
 
 # Estimate alpha metrics and find mean
@@ -198,11 +198,11 @@ mean(S18_alpha$Shannon)
 mean(S18_alpha$Pielou)
 
 
-(plot <- ggplot(S18_alpha, aes(x = Season, y = Chao1, color = Station)) +
+(plot <- ggplot(S18_alpha, aes(x = Season, y = Chao1, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Chao1), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S18_alpha$Chao1), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(S18_alpha$Chao1) - 450, mean(S18_alpha$Chao1) + 450) + # Set y-axis limits
+    ylim(mean(S18_alpha$Chao1) - 550, mean(S18_alpha$Chao1) + 550) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -219,11 +219,11 @@ ggsave(
   plot = plot, width = 6, height = 6, dpi = 300
 )
 
-(plot <- ggplot(S18_alpha, aes(x = Season, y = Shannon, color = Station)) +
+(plot <- ggplot(S18_alpha, aes(x = Season, y = Shannon, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Shannon), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S18_alpha$Shannon), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(S18_alpha$Shannon) -2.5, mean(S18_alpha$Shannon) + 2.5) + # Set y-axis limits
+    ylim(mean(S18_alpha$Shannon) -2.3, mean(S18_alpha$Shannon) + 2.3) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -240,11 +240,11 @@ ggsave(
   plot = plot, width = 6, height = 6, dpi = 300
 )
 
-(plot <- ggplot(S18_alpha, aes(x = Season, y = Pielou, color = Station)) +
+(plot <- ggplot(S18_alpha, aes(x = Season, y = Pielou, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Pielou), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(S18_alpha$Pielou), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(S18_alpha$Pielou) - 0.4, mean(S18_alpha$Pielou) + 0.4) + # Set y-axis limits
+    ylim(mean(S18_alpha$Pielou) - 0.35, mean(S18_alpha$Pielou) + 0.35) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -271,7 +271,7 @@ ggsave(
 (sample_sum_df <- data.frame(sum = sample_sums(COI_physeq_data)) %>% 
    arrange(desc(sum)))
 
-(COI_rarefied_data <- rarefy_even_depth(COI_physeq_data, sample.size = 4000,
+(COI_rarefied_data <- rarefy_even_depth(COI_physeq_data, sample.size = 5000,
                                         rngseed = 199, replace = FALSE, trimOTUs = TRUE, verbose = TRUE))
 
 
@@ -286,11 +286,11 @@ mean(COI_alpha$Shannon)
 mean(COI_alpha$Pielou)
 
 
-(plot <- ggplot(COI_alpha, aes(x = Season, y = Chao1, color = Station)) +
+(plot <- ggplot(COI_alpha, aes(x = Season, y = Chao1, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Chao1), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(COI_alpha$Chao1), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(COI_alpha$Chao1) - 300, mean(COI_alpha$Chao1) + 300) + # Set y-axis limits
+    ylim(mean(COI_alpha$Chao1) - 260, mean(COI_alpha$Chao1) + 260) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -307,11 +307,11 @@ ggsave(
   plot = plot, width = 6, height = 6, dpi = 300
 )
 
-(plot <- ggplot(COI_alpha, aes(x = Season, y = Shannon, color = Station)) +
+(plot <- ggplot(COI_alpha, aes(x = Season, y = Shannon, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Shannon), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(COI_alpha$Shannon), linetype = "dashed", color = "red", size = 1) + # Add mean line
-    ylim(mean(COI_alpha$Shannon) - 2, mean(COI_alpha$Shannon) + 2) + # Set y-axis limits
+    ylim(mean(COI_alpha$Shannon) - 2.2, mean(COI_alpha$Shannon) + 2.2) + # Set y-axis limits
     scale_color_manual(values = c("#0072B2", "#D55E00")) + # Customize colors
     theme_minimal() +
     theme(
@@ -328,7 +328,7 @@ ggsave(
   plot = plot, width = 6, height = 6, dpi = 300
 )
 
-(plot <- ggplot(COI_alpha, aes(x = Season, y = Pielou, color = Station)) +
+(plot <- ggplot(COI_alpha, aes(x = Season, y = Pielou, color = Size.Fraction)) +
     geom_violin(aes(x = Season, y = Pielou), fill = "gray", alpha = 0.5, color = NA) + # Single violin for all samples per Season
     geom_jitter(size = 5, alpha = 0.7, width = 0.1) +
     geom_hline(yintercept = mean(COI_alpha$Pielou), linetype = "dashed", color = "red", size = 1) + # Add mean line
